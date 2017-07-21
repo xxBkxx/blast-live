551fdc43255b45f896761e0628067ea25ad27a7e
xxBkxx <bkdixxon@gmail.com>
2017-07-20 23:36:08 -0400

    server

--- api/server.js
+++ api/server.js
@@ -43,7 +43,7 @@ app.all('*', function(req,res, next){
 	console.log('next');
 	res.redirect("https://" + req.hostname + ":" + app.get('port_https') + req.url);
 	// res.redirect("https://localhost:" + app.get('port_https') + req.url)
-	console.log(req.hostname);
+	console.log(req.url);
 });
 
 var port = process.env.PORT || 8080;
