6363c9e5d537d193a6c776ed1e4ecf4e21a07a34
xxBkxx <bkdixxon@gmail.com>
2017-07-24 19:58:42 -0400

    server

--- api/server.js
+++ api/server.js
@@ -68,7 +68,7 @@ const options = {
 	// rejectUnauthorized: false
 };
 
-var insecureServer = http.createServer(app).listen(8080);
+var insecureServer = http.createServer(app).listen(80);
 
 // var secureServer = https.createServer(options, app,(req,res) => {
 // 	console.log(app);
@@ -77,7 +77,7 @@ var insecureServer = http.createServer(app).listen(8080);
 // 	res.send("hellp world");
 // }).listen(8443);
 
-var secureServer = https.createServer(options, app).listen(8443);
+var secureServer = https.createServer(options, app).listen(443);
 
 mongoose.Promise = global.Promise;
 mongoose.connect('mongodb://bkdixxon:tmmpw0418@ec2-52-40-8-179.us-west-2.compute.amazonaws.com/db');
