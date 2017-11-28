8acb2f0b89deb7f58cdde6c76617dcb82371b59a
xxBkxx <bkdixxon@gmail.com>
2017-11-28 09:34:40 -0500

    path change

--- api/routes/astronaut_routes.js
+++ api/routes/astronaut_routes.js
@@ -12,7 +12,7 @@ var storage = multer.diskStorage({
 	
 	destination: function(req, file, callback){
 		// callback(null, "home/ubuntu/blast-live/app/assets/img/");
-		callback(null, "/home/ubuntu/blast-live/app/site/assets/img/");
+		callback(null, "/../../assets/img/");
 	},
 
 	filename: function(req, file, callback){
